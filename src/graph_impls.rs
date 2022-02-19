d8b81a09dbe3bd09251acfa86e6bc4d42e10cfff
setzer22 <jsanchezfsms@gmail.com>
2022-02-19 10:19:31 +0100

    Initial commit

---
+++ src/graph_impls.rs
@@ -0,0 +1,130 @@
+use super::*;
+
+impl<NodeData, DataType, ValueType> Graph<NodeData, DataType, ValueType> {
+    pub fn new() -> Self {
+        Self {
+            nodes: SlotMap::default(),
+            inputs: SlotMap::default(),
+            outputs: SlotMap::default(),
+            connections: SecondaryMap::default(),
+        }
+    }
+
+    pub fn add_node(
+        &mut self,
+        label: String,
+        user_data: NodeData,
+        f: impl FnOnce(NodeId, &mut Node<NodeData>),
+    ) -> NodeId {
+        let node_id = self.nodes.insert_with_key(|node_id| {
+            Node {
+                id: node_id,
+                label,
+                // These get filled in later by the user function
+                inputs: Vec::default(),
+                outputs: Vec::default(),
+                user_data,
+            }
+        });
+
+        f(node_id, self.nodes.get_mut(node_id).unwrap());
+
+        node_id
+    }
+
+    pub fn remove_node(&mut self, node_id: NodeId) {
+        self.connections
+            .retain(|i, o| !(self.outputs[*o].node == node_id || self.inputs[i].node == node_id));
+        let inputs: SVec<_> = self[node_id].input_ids().collect();
+        for input in inputs {
+            self.inputs.remove(input);
+        }
+        let outputs: SVec<_> = self[node_id].output_ids().collect();
+        for output in outputs {
+            self.outputs.remove(output);
+        }
+        self.nodes.remove(node_id);
+    }
+
+    pub fn remove_connection(&mut self, input_id: InputId) -> Option<OutputId> {
+        self.connections.remove(input_id)
+    }
+
+    pub fn iter_nodes(&self) -> impl Iterator<Item = NodeId> + '_ {
+        self.nodes.iter().map(|(id, _)| id)
+    }
+
+    pub fn add_connection(&mut self, output: OutputId, input: InputId) {
+        self.connections.insert(input, output);
+    }
+
+    pub fn iter_connections(&self) -> impl Iterator<Item = (InputId, OutputId)> + '_ {
+        self.connections.iter().map(|(o, i)| (o, *i))
+    }
+
+    pub fn connection(&self, input: InputId) -> Option<OutputId> {
+        self.connections.get(input).copied()
+    }
+
+    pub fn any_param_type(&self, param: AnyParameterId) -> Result<&DataType, EguiGraphError> {
+        match param {
+            AnyParameterId::Input(input) => self.inputs.get(input).map(|x| &x.typ),
+            AnyParameterId::Output(output) => self.outputs.get(output).map(|x| &x.typ),
+        }
+        .ok_or(EguiGraphError::InvalidParameterId(param))
+    }
+
+    pub fn get_input(&self, input: InputId) -> &InputParam<DataType, ValueType> {
+        &self.inputs[input]
+    }
+
+    pub fn get_output(&self, output: OutputId) -> &OutputParam<DataType> {
+        &self.outputs[output]
+    }
+}
+
+impl<NodeData, DataType, ValueType> Default for Graph<NodeData, DataType, ValueType> {
+    fn default() -> Self {
+        Self::new()
+    }
+}
+
+impl<NodeData> Node<NodeData> {
+    pub fn inputs<'a, DataType, DataValue>(
+        &'a self,
+        graph: &'a Graph<NodeData, DataType, DataValue>,
+    ) -> impl Iterator<Item = &InputParam<DataType, DataValue>> + 'a {
+        self.input_ids().map(|id| graph.get_input(id))
+    }
+
+    pub fn outputs<'a, DataType, DataValue>(
+        &'a self,
+        graph: &'a Graph<NodeData, DataType, DataValue>,
+    ) -> impl Iterator<Item = &OutputParam<DataType>> + 'a {
+        self.output_ids().map(|id| graph.get_output(id))
+    }
+
+    pub fn input_ids(&self) -> impl Iterator<Item = InputId> + '_ {
+        self.inputs.iter().map(|(_name, id)| *id)
+    }
+
+    pub fn output_ids(&self) -> impl Iterator<Item = OutputId> + '_ {
+        self.outputs.iter().map(|(_name, id)| *id)
+    }
+
+    pub fn get_input(&self, name: &str) -> Result<InputId, EguiGraphError> {
+        self.inputs
+            .iter()
+            .find(|(param_name, _id)| param_name == name)
+            .map(|x| x.1)
+            .ok_or_else(|| EguiGraphError::NoParameterNamed(self.id, name.into()))
+    }
+
+    pub fn get_output(&self, name: &str) -> Result<OutputId, EguiGraphError> {
+        self.outputs
+            .iter()
+            .find(|(param_name, _id)| param_name == name)
+            .map(|x| x.1)
+            .ok_or_else(|| EguiGraphError::NoParameterNamed(self.id, name.into()))
+    }
+}
---
+++ src/lib.rs
@@ -0,0 +1,89 @@
+use slotmap::{SecondaryMap, SlotMap};
+
+pub mod id_type;
+pub use id_type::*;
+
+pub mod index_impls;
+
+pub mod graph_impls;
+
+pub mod error;
+pub use error::*;
+
+pub mod ui_state;
+pub use ui_state::*;
+
+pub mod node_finder;
+pub use node_finder::*;
+
+#[cfg(feature = "persistence")]
+use serde::{Deserialize, Serialize};
+
+pub type SVec<T> = smallvec::SmallVec<[T; 4]>;
+
+#[derive(Debug, Clone)]
+#[cfg_attr(feature = "persistence", derive(Serialize, Deserialize))]
+pub struct Node<NodeData> {
+    pub id: NodeId,
+    pub label: String,
+    pub inputs: Vec<(String, InputId)>,
+    pub outputs: Vec<(String, OutputId)>,
+    pub user_data: NodeData,
+}
+
+/// There are three kinds of input params
+#[derive(Debug, Clone, Copy)]
+#[cfg_attr(feature = "persistence", derive(Serialize, Deserialize))]
+pub enum InputParamKind {
+    /// No constant value can be set. Only incoming connections can produce it
+    ConnectionOnly,
+    /// Only a constant value can be set. No incoming connections accepted.
+    ConstantOnly,
+    /// Both incoming connections and constants are accepted. Connections take
+    /// precedence over the constant values.
+    ConnectionOrConstant,
+}
+
+#[cfg(feature = "persistence")]
+fn shown_inline_default() -> bool {
+    true
+}
+
+#[derive(Debug, Clone)]
+#[cfg_attr(feature = "persistence", derive(Serialize, Deserialize))]
+pub struct InputParam<DataType, ValueType> {
+    pub id: InputId,
+    /// The data type of this node. Used to determine incoming connections. This
+    /// should always match the type of the InputParamValue, but the property is
+    /// not actually enforced.
+    pub typ: DataType,
+    /// The constant value stored in this parameter.
+    pub value: ValueType,
+    /// The input kind. See [`InputParamKind`]
+    pub kind: InputParamKind,
+    /// Back-reference to the node containing this parameter.
+    pub node: NodeId,
+    /// When true, the node is shown inline inside the node graph.
+    #[cfg_attr(feature = "persistence", serde(default = "shown_inline_default"))]
+    pub shown_inline: bool,
+}
+
+#[derive(Debug, Clone)]
+#[cfg_attr(feature = "persistence", derive(Serialize, Deserialize))]
+pub struct OutputParam<DataType> {
+    pub id: OutputId,
+    /// Back-reference to the node containing this parameter.
+    pub node: NodeId,
+    pub typ: DataType,
+}
+
+#[derive(Debug, Clone)]
+#[cfg_attr(feature = "persistence", derive(Serialize, Deserialize))]
+pub struct Graph<NodeData, DataType, ValueType> {
+    pub nodes: SlotMap<NodeId, Node<NodeData>>,
+    pub inputs: SlotMap<InputId, InputParam<DataType, ValueType>>,
+    pub outputs: SlotMap<OutputId, OutputParam<DataType>>,
+    // Connects the input of a node, to the output of its predecessor that
+    // produces it
+    connections: SecondaryMap<InputId, OutputId>,
+}
